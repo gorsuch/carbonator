3779033dde2714affa5afc2feebeafbcb0ee2421
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-25 21:34:48 -0500

    much simpler

--- lib/carbinator/parser.rb
+++ lib/carbinator/parser.rb
@@ -1,14 +1,26 @@
 module Carbinator
   class Parser
-    def metricname(data, opts)
-      metricname_key = opts[:metricname_key] || 'metricname'
-      name = data[metricname_key]
+    def metricname(data, k, opts)
+      base_key = opts[:base_key] || 'base'
+      base = data[base_key]
+
+      name = [base, k].join('.')
+
       prefix = opts[:prefix]
       prefix ? "#{prefix}.#{name}" : name
     end
 
+    def filter_numeric(data)
+      data.select { |k,v| v.kind_of?(Numeric) }
+    end
+
     def parse(data, opts={})
-      ["#{metricname(data, opts)} #{data['value']}"]
+      base_key = opts[:base_key] || 'base'
+      base = data[base_key]
+      
+      filter_numeric(data).map do |k,v|
+        "#{metricname(data, k, opts)} #{v}"              
+      end
     end
   end
 end
--- spec/carbinator/parser_spec.rb
+++ spec/carbinator/parser_spec.rb
@@ -1,30 +1,20 @@
 require 'spec_helper'
 
 describe Carbinator::Parser do
-  let(:simple_data) { {'metricname' => 'foo', 'value' => 1 } }
-  let(:complex_data) { {'metricname' => 'foo', 'http_response' => 1, 'connect_time' => 0.2 } }
+  let(:data) { {'base' => 'foo', 'http_code' => 200, 'connect_time' => 0.2} }
   let(:parser) { Carbinator::Parser.new }
 
   describe '#parse' do
     it 'it should return an array' do
-      parser.parse(simple_data).should be_kind_of(Array) 
+      parser.parse(data).should be_kind_of(Array) 
     end
 
     it 'should handle a simple dataset' do
-      parser.parse(simple_data).should eq(["foo 1"])
+      parser.parse(data).should eq(['foo.http_code 200', 'foo.connect_time 0.2'])
     end
 
     it 'should prefix the data if asked' do
-      parser.parse(simple_data, :prefix => 'test').should eq(["test.foo 1"])
-    end
-
-    it 'should use a different value as the metric name if asked' do
-      parser.parse(simple_data.merge('alt' => 'other'), :metricname_key => 'alt').should eq(['other 1'])
-    end
-
-    it 'should handle complex data' do
-      expected = ['foo.http_response 1', 'foo.connect_time 0.2']
-      parser.parse(complex_data).should eq(expected)
+      parser.parse(data, :prefix => 'test').should eq(['test.foo.http_code 200', 'test.foo.connect_time 0.2'])
     end
   end
 end
